fb962ef94dc1cbc7e530037dccf34f056becfe68
BrownOfSummer <l846093185@163.com>
2017-03-31 17:40:12 +0800

    Do auto in different path

--- tensorflow_sample/cnn_text_filter/cnn_text_filter_functions/test_cnn_filter_common.py
+++ tensorflow_sample/cnn_text_filter/cnn_text_filter_functions/test_cnn_filter_common.py
@@ -1,20 +1,80 @@
 import os
 import numpy as np
 from cnn_filter_common import *
-positive_data_file = "../random_data/new_split/t_R1.csv"
-negative_data_file = "../random_data/new_split/t_W1.csv"
 
-#split_text(positive_data_file, negative_data_file)
-#generate_vocab(positive_data_file, negative_data_file)
+DATA_DIR = "../IDs"
+MODEL_DIR = "../MODEL_DIR"
+#path_untrain_r = DATA_DIR + "/id2" + "/TRUE/untrain1"
+#path_untrain_w = DATA_DIR + "/id2" + "/FALSE/untrain1"
 
+query_right = "../random_data/new_split/q_R2.csv"
+query_wrong = "../random_data/new_split/q_W2.csv"
+def run_sys_command(command_str,output_type=""):
+    print("command_str", command_str)
+    import subprocess
+    
+    if output_type == "FILE":
+#         fdout = open("/tmp/tmpSysCommand.out", 'w')
+        fderr = open("/tmp/tmpSysCommand.err", 'w')
+    else:
+        fderr = subprocess.PIPE
+     
+    p = subprocess.Popen(command_str, shell=True, stdout=subprocess.PIPE, stderr=fderr)  
+    p.wait()
+    stdout,stderr = p.communicate()
+    print('stdout : ',stdout)
+    print('stderr : ',stderr)
+    return stdout
 
-#vocab_path = os.path.join("./vocab_dir/", "vocab")
-#vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
-#print(vocab_processor.max_document_length)
-training_r = "./right/training.log"
-training_w = "./wrong/training.log"
-#training_r = "../random_data/new_split/t_R1.csv"
-#training_w = "../random_data/new_split/t_W1.csv"
-#cnn_train(training_r, training_w)
-cnn_retrain(training_r, training_w)
-#cnn_query(positive_data_file, training_r)
+def run_retrain(path_untrain_r, path_untrain_w):
+    print("\nDo the retrain step...\n")
+    model_dir = MODEL_DIR + "/id2"
+    split_text(path_untrain_r, path_untrain_w)
+    append_vocab(model_dir, path_untrain_r, path_untrain_w)
+    training_r = os.path.join(os.path.dirname(path_untrain_r), "training")
+    training_w = os.path.join(os.path.dirname(path_untrain_w), "training")
+    cnn_retrain(model_dir, training_r, training_w)
+
+    """Append the training to trained"""
+    trained_r = training_r.replace("training","trained")
+    trained_w = training_w.replace("training","trained")
+    run_sys_command(("cat %s >> %s")%(training_r, trained_r))
+    run_sys_command(("cat %s >> %s")%(training_w, trained_w))
+
+    """Remove the untrain file"""
+    run_sys_command(("rm %s %s")%(path_untrain_r, path_untrain_w))
+
+def run_train(path_untrain_r, path_untrain_w):
+    print("\nDo the train step...\n")
+    split_text(path_untrain_r, path_untrain_w)
+    generate_vocab(path_untrain_r, path_untrain_w)
+    training_r = os.path.join(os.path.dirname(path_untrain_r), "training")
+    training_w = os.path.join(os.path.dirname(path_untrain_w), "training")
+    cnn_train(training_r, training_w)
+    """Move to MODEL_DIR"""
+    model_dir = os.path.join(os.path.dirname(training_r),"..")
+    runs_dir = os.path.join(model_dir, "runs")
+    vocab_path = os.path.join(model_dir, "vocab")
+    run_sys_command(("mv %s %s")%(runs_dir, MODEL_DIR+"/id2"))
+    run_sys_command(("mv %s %s")%(vocab_path, MODEL_DIR+"/id2"))
+
+    """Append the training to trained"""
+    trained_r = training_r.replace("training","trained")
+    trained_w = training_w.replace("training","trained")
+    run_sys_command(("cat %s >> %s")%(training_r, trained_r))
+    run_sys_command(("cat %s >> %s")%(training_w, trained_w))
+
+    """Remove the untrain file"""
+    run_sys_command(("rm %s %s")%(path_untrain_r, path_untrain_w))
+
+for text in ["untrain1", "untrain2", "untrain3"]:
+    path_untrain_r = DATA_DIR + "/id2/TRUE/" + text
+    path_untrain_w = DATA_DIR + "/id2/FALSE/" + text
+    if os.path.exists(MODEL_DIR + "/id2/runs"):
+        run_retrain(path_untrain_r, path_untrain_w)
+    else:
+        run_train(path_untrain_r, path_untrain_w)
+
+    cnn_query(MODEL_DIR+"/id2", query_right)
+    print("query split ====================")
+    cnn_query(MODEL_DIR+"/id2", query_wrong)
